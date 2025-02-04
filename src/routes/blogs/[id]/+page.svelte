04c93b8404205d1f405773ddc858bec7d0ae5085
zachhh14 <zactec115@gmail.com>
2025-02-04 22:47:51 +0800

    fix: remove unused lines

--- src/routes/blogs/[id]/+page.svelte
+++ src/routes/blogs/[id]/+page.svelte
@@ -6,4 +6,3 @@
 <h1 class="text-6xl font-bold text-center">{data.title}</h1>
 <div class="w-full text-sm font-bold text-center text-gray-700">Author: {data.submitted_by ?? 'Anonymous'}</div>
 <p class="mt-5">{data.message}</p>
-lorem*25
