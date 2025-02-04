550cd4cd23be009ec4b311393276c10a9d0e8b95
zachhh14 <zactec115@gmail.com>
2025-02-04 22:46:43 +0800

    feat: fix styling in single view of page

--- src/routes/blogs/[id]/+page.svelte
+++ src/routes/blogs/[id]/+page.svelte
@@ -1,7 +1,9 @@
 <script>
-	/** @type {import('./$types').PageProps} */
-	let { data } = $props();
+    /** @type {import('./$types').PageProps} */
+    let { data } = $props()
 </script>
 
-<h1>{data.title}</h1>
-<div>{@html data.message}</div>
+<h1 class="text-6xl font-bold text-center">{data.title}</h1>
+<div class="w-full text-sm font-bold text-center text-gray-700">Author: {data.submitted_by ?? 'Anonymous'}</div>
+<p class="mt-5">{data.message}</p>
+lorem*25
