e16b7051905fa9ad6eeab456867a94297b16441d
zachhh14 <zactec115@gmail.com>
2025-02-07 15:26:12 +0800

    fix: remove server side rendering

--- src/routes/blogs/[id]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    /** @type {import('./$types').PageProps} */
-    let { data } = $props()
-</script>
-
-<h1 class="text-6xl font-bold text-center">{data.title}</h1>
-<div class="w-full text-sm font-bold text-center text-gray-700">Author: {data.submitted_by ?? 'Anonymous'}</div>
-<p class="mt-5">{data.message}</p>
